d98e371ac7949bf38d1ffb5f2dc5219edf523881
Lucas Silva <lucassilvaee1245@gmail.com>
2022-05-03 16:15:44 -0300

    prefizUrl

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { CotacaoTDO, CotacaoTDOPayload, DescontoGeral } from "./types";
 
-export const apiEndPoint = 'success.vps-kinghost.net:3050';
+export const apiEndPoint = 'htpp://success.vps-kinghost.net:3050';
 
 export const api = axios.create({
 	baseURL: apiEndPoint,
