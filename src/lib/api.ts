a2042d60f7e13b1b3aabbac482c84eaecc1d9b1b
Lucas Silva <lucassilvaee1245@gmail.com>
2022-06-27 17:53:14 -0300

    nome da empresa solicitante

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,8 +1,8 @@
 import axios from "axios";
 import { CotacaoTDO, CotacaoTDOPayload, DescontoGeral } from "./types";
 
-//export const apiEndPoint = 'https://apicotacaoteste.successsistemas.com';
-export const apiEndPoint = 'http://localhost:3051';
+export const apiEndPoint = 'https://apicotacaoteste.successsistemas.com';
+//export const apiEndPoint = 'http://localhost:3051';
 //lembrar de alterar a porta na api
 export const api = axios.create({
 	baseURL: apiEndPoint,
